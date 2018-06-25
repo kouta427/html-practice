18dc027f1473e163652a4c703b5d88b705b5431a
Your Name <you@example.com>
2018-06-25 23:41:19 +0900

    test

---
+++ up.bat
@@ -0,0 +1,8 @@
+
+git add --all .
+set /P USR_INPUT_STR="•¶Žš—ñ‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: "
+git commit -m %USR_INPUT_STR%
+git remote add origin https://github.com/kouta427/html-practice.git
+git push -u origin master
+
+pause
